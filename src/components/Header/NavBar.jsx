ab0000e5db4820dd43082e90941b214f155d0d19
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-12-26 20:34:53 +0600

    3rd commit

--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router';
 
 const NavBar = () => {
+      const links =<>
+      <li><NavLink to="/">Home</NavLink></li>
+       <li><NavLink to="/">Login</NavLink></li>
+        <li><NavLink to="/">Logout</NavLink></li>
+      
+      </>
       return (
             <div>
                   <div className="navbar bg-base-100 shadow-sm">
@@ -12,32 +19,23 @@ const NavBar = () => {
       <ul
         tabIndex="-1"
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
+        <li><a>{links}</a></li>
         <li>
-          <a>Parent</a>
+         
           <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
+           
           </ul>
         </li>
-        <li><a>Item 3</a></li>
+        
       </ul>
     </div>
     <a className="btn btn-ghost text-xl">daisyUI</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2 bg-base-100 w-40 z-1">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+     {
+      links
+     }
     </ul>
   </div>
   <div className="navbar-end">
