eb30c4e427677513b6663d7fc8a0d157e49f1288
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-12-26 20:20:43 +0600

    2nd commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Home from './components/Home/Home.jsx';
+import Root from './Layout/Root.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component:Root,
+    children:[
+      {
+        index:true,
+        Component:Home
+      }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+   
+     <RouterProvider router={router} />
+
+  </StrictMode>
 )
